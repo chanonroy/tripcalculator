4a6cb4d78666f51d13fe30445cf2980b591fa870
Chanon Roy <chanonroy@gmail.com>
2016-10-11 23:27:05 -0400

    XML parsing for Miles Per Gallon on Primary Key ID car

--- app.py
+++ app.py
@@ -21,17 +21,29 @@ def get_id():
     r = requests.get(url)
     root = ET.fromstring(r.content)
 
-    version = root[0][0].text       # Auto 5-spd, 4 cyl, 2.4 L
-    vehicle_id = root[0][1].text    # 21960
+    version = root[0][0].text       # 'Auto 5-spd, 4 cyl, 2.4 L'
+    vehicle_id = root[0][1].text    # '21960'
 
     # Need to properly iterate through all car options, not just first (TODO)
-    return vehicle_id, version
-
-print(get_id())
+    return vehicle_id
 
 
 def get_gas():
     """ Get Gas info from fueleconomy.gov XML API """
+
+    vehicle_id = 21960 # get_id()
+
+    url = "http://www.fueleconomy.gov/ws/rest/ympg/shared/ympgVehicle/{}".format(vehicle_id)
+    r = requests.get(url)
+    root = ET.fromstring(r.content)
+
+    avg_mpg = root[0].text         # 27.89997198
+    max_mpg = root[3].text         # 38
+    min_mpg = root[4].text         # 20
+    
+    print(avg_mpg)
+
+get_gas()
 
 
 def uber():
